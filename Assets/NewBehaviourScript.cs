cb13f486301a60a257bcebafffe9ed8d33bc1fa4
Moosan <washer2016@outlook.jp>
2019-05-16 11:32:27 +0900

    AddProject

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject obj;
+    private void Awake()
+    {
+        Time.timeScale = 1f;
+    }
+    private void Start()
+    {
+        StartCoroutine(tapioka());
+    }
+    IEnumerator tapioka()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.3f);
+            Instantiate(obj, transform.position, transform.rotation);
+        }
+    }
+}
